41cf81d1c24895f85692c09b6323bc3ec7a1992d
Alex Drubezki <alexdru81@gmail.com>
2023-07-11 10:13:50 +0300

    aaaa

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,9 @@ const inputValidator = require('../middlewares/input-validation')
 const { addSymbol } = require('../controllers/users/controller');
 const { addSymbolValidator } = require('../controllers/users/validator');
 
+//encode urls for all endpoints of this router only
+//app.use(express.urlencoded({ extended: false }));
+
 router.get('/dashboard', (req, res) => {
     res.send('dashboard');
 })
@@ -14,6 +17,8 @@ router.get('/logout', (req, res) => {
     res.send('logout');
 })
 
+//this way we add url encoded only for the specific call '/symbol'
+//router.post('/symbol', express.urlencoded({ extended: false }), inputValidator(addSymbolValidator), addSymbol)
 router.post('/symbol', inputValidator(addSymbolValidator), addSymbol)
 
 module.exports = router;
